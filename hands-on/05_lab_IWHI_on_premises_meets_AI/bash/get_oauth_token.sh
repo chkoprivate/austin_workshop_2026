e39f8ec180672b4c88c764a1e69b5875e504c0b6
Christian Kopecki <Christian.Kopecki@ibm.com>
2026-04-27 16:59:03 -0500

    Update get_oauth_token.sh

--- hands-on/05_lab_IWHI_on_premises_meets_AI/bash/get_oauth_token.sh
+++ hands-on/05_lab_IWHI_on_premises_meets_AI/bash/get_oauth_token.sh
@@ -11,25 +11,8 @@
 # - OAuth client already registered in Integration Server
 # -------------------------------------------------------------------
 
-# --- Configuration (EDIT THESE VALUES) ------------------------------
-
-IS_BASE_URL="http://localhost:5555"
-CLIENT_ID="<YOUR CLIENT_ID>"
-CLIENT_SECRET="<YOUR CLIENT_SECRET>"
-SCOPES="customer:read wxmcp.server"
-
-# -------------------------------------------------------------------
-
-echo "Requesting OAuth Bearer Token from Integration Server..."
-echo
-
-curl -s -X POST "${IS_BASE_URL}/invoke/pub.oauth:getAccessToken" \
-  -u "${CLIENT_ID}:${CLIENT_SECRET}" \
+curl -s -X POST "http://localhost:5555/invoke/pub.oauth:getAccessToken" \
+  -u "<YOUR CLIENT_ID>"<YOUR CLIENT_SECRET>" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=client_credentials" \
-  -d "scope=${SCOPES}"
-
-echo
-echo
-echo "If an access_token is returned, copy it and use it as:"
-echo "Authorization: Bearer <access_token>"
+  -d "scope=customer:read wxmcp.server"
